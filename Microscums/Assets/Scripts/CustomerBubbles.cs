5548201e237d1730c65a6aded9eef9a724e10186
Andrew Steven Chau <aschau24@gmail.com>
2017-01-22 11:46:51 -0800

    fixed customerbubble detection

--- Microscums/Assets/Scripts/CustomerBubbles.cs
+++ Microscums/Assets/Scripts/CustomerBubbles.cs
@@ -27,35 +27,35 @@ public class CustomerBubbles : MonoBehaviour {
             keepTrack = timeDelay;
             string item = orders.order_list[orders.item_index]; //retrieve item from list
             orders.item_index++; //set index to grab next item in next loops
-            if (this.transform.GetChild(0).name != "SpeechBubble(Clone)"){
-                GameObject custbub = Instantiate(bubble, transform.position, transform.rotation, parent.transform);
-                custbub.transform.SetAsFirstSibling();
-            }
+            //if (this.transform.GetChild(0).name != "SpeechBubble(Clone)"){
+            //    GameObject custbub = Instantiate(bubble, transform.position, transform.rotation, parent.transform);
+            //    custbub.transform.SetAsFirstSibling();
+            //}
             if (item == "cupNoodle") {
                 food_type = "cupNoodle";
-                this.transform.GetChild(1).gameObject.SetActive(true);
-                List<int> to_set = new List<int> { 2, 3, 4 };
+                this.transform.GetChild(0).gameObject.SetActive(true);
+                List<int> to_set = new List<int> { 1, 2, 3 };
                 set_others_to_false(to_set);
             }
             if (item == "hotPocket")
             {
                 food_type = "hotPocket";
-                this.transform.GetChild(2).gameObject.SetActive(true);
-                List<int> to_set = new List<int> { 1, 3, 4 };
+                this.transform.GetChild(1).gameObject.SetActive(true);
+                List<int> to_set = new List<int> { 0, 2, 3 };
                 set_others_to_false(to_set);
             }
             if (item == "popcorn")
             {
                 food_type = "popcorn";
-                this.transform.GetChild(3).gameObject.SetActive(true);
-                List<int> to_set = new List<int> { 1, 2, 4 };
+                this.transform.GetChild(2).gameObject.SetActive(true);
+                List<int> to_set = new List<int> { 0, 1, 3 };
                 set_others_to_false(to_set);
             }
             if (item == "tvDinner")
             {
                 food_type = "tvDinner";
-                this.transform.GetChild(4).gameObject.SetActive(true);
-                List<int> to_set = new List<int> { 1, 2, 3 };
+                this.transform.GetChild(3).gameObject.SetActive(true);
+                List<int> to_set = new List<int> { 0, 1, 2 };
                 set_others_to_false(to_set);
             }
         }
