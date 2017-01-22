a1eacfa29a36e25c2d1aa292e2bb09fd6f44136b
Andrew Steven Chau <aschau24@gmail.com>
2017-01-21 23:49:37 -0800

    added trash can detection. changed to world space.

--- Microscums/Assets/Scripts/foodItem.cs
+++ Microscums/Assets/Scripts/foodItem.cs
@@ -12,7 +12,7 @@ public abstract class foodItem : MonoBehaviour {
     public string name = "";
     public int occupied = 0;
     public float trashcanX1, trashcanX2, trashcanY1, trashcanY2;
-    public GameObject control;
+    public GameObject control, mainCamera;
 
     private float offsetX, offsetY;
     
@@ -22,11 +22,12 @@ public abstract class foodItem : MonoBehaviour {
     public virtual void Awake()
     {
         this.trashcan = GameObject.Find("trashcan");
-        this.control = GameObject.Find("microwaveControl");
-        this.trashcanX1 = this.trashcan.transform.position.x - (this.trashcan.GetComponent<RectTransform>().rect.width / 2);
-        this.trashcanX2 = this.trashcan.transform.position.x + (this.trashcan.GetComponent<RectTransform>().rect.width / 2);
-        this.trashcanY1 = this.trashcan.transform.position.y - (this.trashcan.GetComponent<RectTransform>().rect.height / 2);
-        this.trashcanY2 = this.trashcan.transform.position.y + (this.trashcan.GetComponent<RectTransform>().rect.height / 2);
+        this.mainCamera = GameObject.Find("Main Camera");
+        //this.control = GameObject.Find("microwaveControl");
+        //this.trashcanX1 = this.trashcan.transform.position.x - (this.trashcan.GetComponent<RectTransform>().rect.width / 2);
+        //this.trashcanX2 = this.trashcan.transform.position.x + (this.trashcan.GetComponent<RectTransform>().rect.width / 2);
+        //this.trashcanY1 = this.trashcan.transform.position.y - (this.trashcan.GetComponent<RectTransform>().rect.height / 2);
+        //this.trashcanY2 = this.trashcan.transform.position.y + (this.trashcan.GetComponent<RectTransform>().rect.height / 2);
         this.origin = this.transform.position;
     }
 
@@ -53,57 +54,65 @@ public abstract class foodItem : MonoBehaviour {
     {
         this.transform.position = new Vector3(Input.mousePosition.x + offsetX, Input.mousePosition.y + offsetY);
     }
-
+    public virtual RaycastHit2D checkHit(GameObject camera)
+    {
+        return Physics2D.Raycast(new Vector2(camera.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition).origin.x, camera.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition).origin.y), new Vector2(camera.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition).direction.x, camera.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition).direction.y));
+    }
     public virtual void endDrag()
     {
         this.beingDragged = false;
-        if ((this.transform.position.x < this.trashcanX2) && (this.transform.position.x > this.trashcanX1))
+        RaycastHit2D hit = checkHit(this.mainCamera);
+        if (hit)
         {
-            if ((this.transform.position.y < this.trashcanY2) && (this.transform.position.y > this.trashcanY1))
-            {
-                Debug.Log(this.occupied);
-                if (this.occupied == 1)
-                {
-                    this.control.GetComponent<microControl>().space1 = false;
-                    this.control.GetComponent<microControl>().spaces += 1;
-                }
-                else if (this.occupied == 2)
-                {
-                    this.control.GetComponent<microControl>().space2 = false;
-                    this.control.GetComponent<microControl>().spaces += 1;
-                }
-                else if (this.occupied == 3)
-                {
-                    this.control.GetComponent<microControl>().space3 = false;
-                    this.control.GetComponent<microControl>().spaces += 1;
-                }
-                else if (this.occupied == 12)
-                {
-                    this.control.GetComponent<microControl>().space1 = false;
-                    this.control.GetComponent<microControl>().space2 = false;
-                    this.control.GetComponent<microControl>().spaces += 2;
-                }
-                else if (this.occupied == 23)
-                {
-                    this.control.GetComponent<microControl>().space2 = false;
-                    this.control.GetComponent<microControl>().space3 = false;
-                    this.control.GetComponent<microControl>().spaces += 2;
-                }
-                else if (this.occupied == 123)
-                {
-                    this.control.GetComponent<microControl>().space1 = false;
-                    this.control.GetComponent<microControl>().space2 = false;
-                    this.control.GetComponent<microControl>().space3 = false;
-                    this.control.GetComponent<microControl>().spaces += 3;
-                }
-                Destroy(this.gameObject);
-            }
+            Destroy(this.gameObject);
         }
-        else
-        {
-            this.transform.position = origin;
-        }
-        
+    //    if ((this.transform.position.x < this.trashcanX2) && (this.transform.position.x > this.trashcanX1))
+    //    {
+    //        if ((this.transform.position.y < this.trashcanY2) && (this.transform.position.y > this.trashcanY1))
+    //        {
+    //            Debug.Log(this.occupied);
+    //            if (this.occupied == 1)
+    //            {
+    //                this.control.GetComponent<microControl>().space1 = false;
+    //                this.control.GetComponent<microControl>().spaces += 1;
+    //            }
+    //            else if (this.occupied == 2)
+    //            {
+    //                this.control.GetComponent<microControl>().space2 = false;
+    //                this.control.GetComponent<microControl>().spaces += 1;
+    //            }
+    //            else if (this.occupied == 3)
+    //            {
+    //                this.control.GetComponent<microControl>().space3 = false;
+    //                this.control.GetComponent<microControl>().spaces += 1;
+    //            }
+    //            else if (this.occupied == 12)
+    //            {
+    //                this.control.GetComponent<microControl>().space1 = false;
+    //                this.control.GetComponent<microControl>().space2 = false;
+    //                this.control.GetComponent<microControl>().spaces += 2;
+    //            }
+    //            else if (this.occupied == 23)
+    //            {
+    //                this.control.GetComponent<microControl>().space2 = false;
+    //                this.control.GetComponent<microControl>().space3 = false;
+    //                this.control.GetComponent<microControl>().spaces += 2;
+    //            }
+    //            else if (this.occupied == 123)
+    //            {
+    //                this.control.GetComponent<microControl>().space1 = false;
+    //                this.control.GetComponent<microControl>().space2 = false;
+    //                this.control.GetComponent<microControl>().space3 = false;
+    //                this.control.GetComponent<microControl>().spaces += 3;
+    //            }
+    //            Destroy(this.gameObject);
+    //        }
+    //    }
+    //    else
+    //    {
+    //        this.transform.position = origin;
+    //    }
+
     }
 
 }
