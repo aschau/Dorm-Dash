b5711557129f0f22d56d3d841169775109db94d2
Jasminesaur <jnjasmine@yahoo.com>
2017-01-22 16:20:38 -0800

    Fixed game over and main menu

---
+++ Microscums/Assets/instructions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class instructions : MonoBehaviour {
+	GameObject instruct;
+	// Use this for initialization
+	void Start () {
+		this.instruct = GameObject.Find ("Instructions");
+		this.instruct.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void onClick()
+	{
+		this.instruct.gameObject.SetActive (!this.instruct.gameObject.activeSelf);
+	}
+}
